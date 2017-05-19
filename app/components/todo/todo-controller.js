50d48a426af202989aa8552ac0d5e650ae2819b3
Kaleb Hundersmarck <kalebaaron@live.com>
2017-05-18 22:01:18 -0600

    fixed todos

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,48 +1,39 @@
-function TodoController(){
+function TodoController() {
 	// new up the TodoService that has already been configured for your use
 	// There are two methods getTodos returns and array
 	// saveTodos accepts an array and stores it to your local storage
-var todoService = new TodoService ()
-var newToDoList = document.getElementById("newToDoList")
+	var todoService = new TodoService();
 
-// this.todoService = function(e){
-// 	e.preventDefault()
-	
-// }
+	function drawTodos(data) {
+		var elem = document.getElementById('newToDoList');
+		var template = '';
+		var todoTemplate = '';
+		for (var i = 0; i < data.length; i++) {
+			var todo = data[i];
+			template += `
+					${todo} <button type="button" onclick="app.controllers.todoController.removeTodo('${todo}')">Remove</button><br>
+				`
+		}
 
-var toDoList = []
+		elem.innerHTML = template + todoTemplate;
+	}
 
-function MakeToDo(str) {
-  this.str = str,
-  toDoList.push(this)
-  this.createToDo()
-}
+	this.addTodoFromForm = function (e) {
+		e.preventDefault();
+		var form = e.target;
 
-function drawToDos(arrToDos){
-  var template = '<ul>'
-  for(var i= 0; i<arrToDos.length; i++){
-    var toDo = arrToDos[i];
-    template +=`
-    <ul>
-    <li>${toDo.str}</li><button type="button" onclick="removeToDo(${toDo.str})">Remove</button>
-    </ul>
-    `
-  }
-  template += '</ul>'
-  newToDoList.innerHTML = template
-}
+		var todosArray = todoService.getTodos(form.newToDo.value);
+		todosArray.push(form.newToDo.value);
+		todoService.saveTodos(todosArray);
+		todosList = todosArray
+		drawTodos(todosArray);
+	}
 
-this.removeToDo =function removeToDo(str){
-  toDoList.splice(str, 1)
-}
-
-
-this.createToDo = function createToDo(e) {
-  e.preventDefault();
-  var form = e.target
-  new MakeToDo(form.newToDo.value)
-  form.reset()
-  drawToDos(toDoList)
-}
-
+	this.removeTodo = function removeTodo(todo) {
+		var todosArray = todoService.getTodos(todo);
+		todosArray.splice(todo, 1);
+		todoService.saveTodos(todosArray);
+		todosList = todosArray
+		drawTodos(todosArray);
+	}
 }
